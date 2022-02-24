06b25449b1b30a6ec59d04635e1f4c107609ccac
dgonza08 <dgonza08@estudiantes.unileon.es>
2022-02-24 17:44:44 +0000

    Creado directorio para la practica 1 de seguridad

--- SeguridadInformatica/Practica1/Practica1.java
+++ SeguridadInformatica/Practica1/Practica1.java
@@ -3,9 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class main {
+public class Practica1 {
     public static void main(String[] args) throws FileNotFoundException {
-        File archivo = new File("/home/drox/repositorios/Codigo3Informatica/SeguridadInformatica/datos_1.txt");
+        File archivo = new File("/home/drox/repositorios/Codigo3Informatica/SeguridadInformatica/Practica1/datos_1Original.txt");
         Scanner sc = new Scanner(archivo);
 
         String cadena = "";
@@ -16,6 +16,12 @@ public class main {
 
         float total = cadena.length();
 
+        int contadorSaltosLinea = 0;
+        while(sc.hasNextLine()) {
+            contadorSaltosLinea++;
+            sc.nextLine();
+        }
+
         HashMap<Character, Float> mapa = new HashMap<Character, Float>();
         for (int j = 0; j < cadena.length(); j++) {
             Character caracter = cadena.charAt(j);
@@ -23,18 +29,20 @@ public class main {
             for (int k = 0; k < cadena.length(); k++) {
                 if (caracter == cadena.charAt(k)) {
                     contador++;
-                } else if (caracter == '\n') {
+                } else if (caracter == ' '+' ') {
                     contador += 2;
                 }
                 mapa.put(caracter, contador);
             }
         }
 
+        System.out.println("Mapa es: \n" + mapa + "\n");
+
         // Frecuencia absoluta d
         System.out.println("La frecuendia absoluta de d es: " + mapa.get('d'));
-        
-        //Probabilidad de d
-        System.out.println("La probabilidad de d es: " + mapa.get('d') / total);
+
+        // Probabilidad de d
+        System.out.println("La probabilidad de d es: " + mapa.get('d') / (total + (contadorSaltosLinea * 2)));
 
         sc.close();
     }
