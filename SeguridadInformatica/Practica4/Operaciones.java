864371b8039548b1ae0cd2a37bd3c4a693783e84
dgonza08 <dgonza08@estudiantes.unileon.es>
2022-03-22 10:49:24 +0100

    Acabada parte a del primer ejercicio de la practica 4

--- SeguridadInformatica/Practica4/Operaciones.java
+++ SeguridadInformatica/Practica4/Operaciones.java
@@ -1,3 +1,13 @@
 public class Operaciones {
-    
+    public Operaciones() {
+    }
+
+    public float log(float x, float base) {
+        return (float) (Math.log10(x) / Math.log10(base));
+    }
+
+    public String toBinario(long entero){
+        String binario = Long.toBinaryString(entero);
+        return binario;
+    }
 }
--- SeguridadInformatica/Practica4/main.java
+++ SeguridadInformatica/Practica4/main.java
@@ -3,33 +3,61 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class main{
+public class main {
     public static void main(String[] args) throws FileNotFoundException {
         Operaciones op = new Operaciones();
         File f = new File("./datos.txt");
         Scanner sc = new Scanner(f);
 
         String linea = "";
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             linea = sc.nextLine();
         }
 
         String[] datos = linea.split("");
-        for (int i = 0; i < datos.length; i++) {
-            System.out.println(datos[i]);
-        }
 
-        HashMap<String, Float> mapa = new HashMap<String, Float>();
+        HashMap<String, Long> mapa = new HashMap<String, Long>();
         for (int j = 0; j < datos.length; j++) {
             String caracter = datos[j];
-            Float contador = 0f;
+            long valor = j;
             for (int k = 0; k < datos.length; k++) {
-                if (caracter == datos[k]) {
-                    contador++;
-                }
-                mapa.put(caracter, contador);
+                mapa.put(caracter, valor);
             }
         }
 
+        double longMinima = Math.ceil(op.log(mapa.size(), 2)); // se redondea hacia arriba
+
+        System.out.println("Longitud minima: " + longMinima);
+
+        HashMap<String, String> mapaBinario= new HashMap<String, String>();
+
+        for (String key : mapa.keySet()) {
+            mapaBinario.put(key, op.toBinario(mapa.get(key)));
+        }
+
+        for (String key : mapaBinario.keySet()) {
+            double anyadir = longMinima - mapaBinario.get(key).length();
+            for (int i = 0; i < anyadir; i++) {
+                mapaBinario.put(key, "0" + mapaBinario.get(key));
+            }
+        }
+
+        for(String key : mapaBinario.keySet()){
+            System.out.println(key + ": " + mapaBinario.get(key));
+        }
+
+        String cadena = "c!";
+        String[] cadenaSplit = cadena.split("");
+
+        String codificacion = "";
+
+        for (int i = 0; i < cadenaSplit.length; i++) {
+            for(String key: mapaBinario.keySet()){
+                if (key.equals(cadenaSplit[i])) {
+                    codificacion += mapaBinario.get(key);
+                } 
+            }
+        }
+        System.out.println("Codificacion lineal: " + codificacion);
     }
 }
